a4a24623d948e193c7253b535b97f354075f8192
WangChengwen <wangchengwen@WangChengwens-Mac-mini.local>
2014-06-23 20:42:04 +0800

    Initial Commit

---
+++ Shutterbug/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Shutterbug
+//
+//  Created by WangChengwen on 6/23/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
